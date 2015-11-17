68516d50fb124fca3db938a44b21eb4b1ceba5e3
Javier Diaz <javierdiaz@Javiers-MacBook-Pro.local>
2015-11-17 15:53:19 -0300

    arreglo chico

--- src/server.c
+++ src/server.c
@@ -13,8 +13,10 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <netdb.h>
 #include <unistd.h>
 #include <string.h>
+#include <strings.h>
 #include <stdlib.h>
 #include "server.h"
 
